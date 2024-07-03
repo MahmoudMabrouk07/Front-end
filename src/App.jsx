af0c6a6ea64acf74814062b2fe41cbae0514e04f
unknown <mahmoudmabrouk@polytechnicien.tn>
2024-07-03 16:19:12 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+import Sidebar from './Sidebar'
+import Home from './Home'
+
+function App() {
+
+  return (
+    
+      <div>aaaaaaaa</div>
+    
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BsFillBellFill, BsFillEnveloperFill, BspersonCircle, BsSearch, BsJustify } from "react-icons/bs";
+
+
+function Header() {
+    return (
+        <header className="header">
+            <div className='menu-icon'>
+                <BsJustify className="icon"/>
+                aaaaa
+            </div>
+
+
+
+        </header>
+        
+    )
+}
+
+export default Header
